69e78c32643b53b2a4c950d8e25742a1504c7970
TrueAbastien <bastien.legoy@gmail.com>
2019-11-20 12:13:46 +0100

    w o r k s 2

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -145,126 +145,18 @@ Mesh::Mesh(const char* fname) {
 				  iBuf[ii + 2],		useTextures ? uBuf[ii + 2] : iBuf[ii + 2],		// c, ct
 				  is3DTex);
 	  }
-	  //while (!fin.eof()) {
-	  //  std::getline(fin, line);
-		//__countUp++; //DEBUG
-	  //  if (line.find("v ") == 0) {
-	  //    std::stringstream ss(line.c_str() + 2);
-	  //    float x, y, z;
-	  //    ss >> x >> y >> z;
-	  //    vert_palette.push_back(x);
-	  //    vert_palette.push_back(y);
-	  //    vert_palette.push_back(z);
-	  //  } else if (line.find("vt ") == 0) {
-	  //    std::stringstream ss(line.c_str() + 3);
-	  //    float u, v, w;
-	  //    ss >> u >> v >> w;
-	  //    uv_palette.push_back(u);
-	  //    uv_palette.push_back(v);
-	  //    if (!ss.fail()) {
-	  //      uv_palette.push_back(w);
-	  //      is3DTex = true;
-	  //    }
-	  //  } else if (line.find("c ") == 0) {
-	  //    uint32_t a = 0, b = 0, c = 0;
-	  //    if (line[2] == '*') {
-	  //      const uint32_t v_ix = (uint32_t)vert_palette.size() / 3;
-	  //      a = v_ix - 2; b = v_ix - 1; c = v_ix;
-	  //    } else {
-	  //      std::stringstream ss(line.c_str() + 2);
-	  //      ss >> a >> b >> c;
-	  //    }
-	  //    const Vector3 v1(&vert_palette[(a - 1) * 3]);
-	  //    const Vector3 v2(&vert_palette[(b - 1) * 3]);
-	  //    const Vector3 v3(&vert_palette[(c - 1) * 3]);
-	  //    colliders.push_back(Collider(v1, v2, v3));
-	  //  } else if (line.find("f ") == 0) {
-	  //    //Count the slashes
-	  //    int num_slashes = 0;
-	  //    size_t last_slash_ix = 0;
-	  //    bool doubleslash = false;
-	  //    for (size_t i = 0; i < line.size(); ++i) {
-	  //      if (line[i] == '/') {
-	  //        line[i] = ' ';
-	  //        if (last_slash_ix == i - 1) {
-	  //          assert(vert_palette.size() == uv_palette.size() || uv_palette.empty());
-	  //          doubleslash = true;
-	  //        }
-	  //        last_slash_ix = i;
-	  //        num_slashes++;
-	  //      }
-	  //    }
-		//  //std::cout << std::to_string(__countUp) + " " + std::to_string(num_slashes) << std::endl; //DEBUG
-	  //    uint32_t a=0, b=0, c=0, d=0;
-	  //    uint32_t at=0, bt=0, ct=0, dt=0;
-	  //    uint32_t _tmp;
-	  //    std::stringstream ss(line.c_str() + 2);
-	  //    const bool wild = (line[2] == '*');
-	  //    const bool wild2 = (line[3] == '*');
-	  //    bool isQuad = false;
-	  //
-	  //    //Interpret face based on slash
-	  //    if (wild) {
-	  //      assert(num_slashes == 0);
-	  //      const uint32_t v_ix = (uint32_t)vert_palette.size() / 3;
-	  //      const uint32_t t_ix = (uint32_t)uv_palette.size() / (is3DTex ? 3 : 2);
-	  //      if (wild2) {
-	  //        a = v_ix - 3; b = v_ix - 2; c = v_ix - 1; d = v_ix - 0;
-	  //        at = t_ix - 3; bt = t_ix - 2; ct = t_ix - 1; dt = t_ix - 0;
-	  //        isQuad = true;
-	  //      } else {
-	  //        a = v_ix - 2; b = v_ix - 1; c = v_ix;
-	  //        at = t_ix - 2; bt = t_ix - 1; ct = t_ix;
-	  //      }
-	  //    } else if (num_slashes == 0) {
-	  //      ss >> a >> b >> c >> d;
-	  //      at = a; bt = b; ct = c; dt = d;
-	  //      if (!ss.fail()) {
-	  //        isQuad = true;
-	  //      }
-	  //    } else if (num_slashes == 3) {
-	  //      ss >> a >> at >> b >> bt >> c >> ct;
-	  //    } else if (num_slashes == 4) {
-	  //      isQuad = true;
-	  //      ss >> a >> at >> b >> bt >> c >> ct >> d >> dt;
-	  //    } else if (num_slashes == 6) {
-	  //      if (doubleslash) {
-	  //        ss >> a >> _tmp >> b >> _tmp >> c >> _tmp;
-	  //        at = a; bt = b; ct = c;
-	  //      } else {
-	  //        ss >> a >> at >> _tmp >> b >> bt >> _tmp >> c >> ct >> _tmp;
-	  //      }
-	  //    } else if (num_slashes == 8) {
-	  //      isQuad = true;
-	  //      if (doubleslash) {
-	  //        ss >> a >> _tmp >> b >> _tmp >> c >> _tmp >> d >> _tmp;
-	  //        at = a; bt = b; ct = c; dt = d;
-	  //      } else {
-	  //        ss >> a >> at >> _tmp >> b >> bt >> _tmp >> c >> ct >> _tmp >> d >> dt >> _tmp;
-	  //      }
-	  //    } else {
-	  //      assert(false);
-	  //      continue;
-	  //    }
-	  //
-	  //    //Add face to list
-	  //    AddFace(vert_palette, uv_palette, a, at, b, bt, c, ct, is3DTex);
-	  //    if (isQuad) {
-	  //      AddFace(vert_palette, uv_palette, c, ct, d, dt, a, at, is3DTex);
-	  //    }
-		//  
-		//  // Materials
-		//} else if (line.find("mtllib ") == 0) {
-		//	isUsingMaterials = true;
-		//	std::stringstream ss(line.c_str() + 7);
-		//	ss >> mtl_file;
-		//} else if (line.find("usemtl ") == 0 && isUsingMaterials) {
-		//	std::string tname;
-		//	std::stringstream ss(line.c_str() + 7);
-		//	ss >> tname;
-		//	material_name_palette.push_back(tname);
-		//}
-	  //}
+
+	//  // Materials
+	//} else if (line.find("mtllib ") == 0) {
+	//	isUsingMaterials = true;
+	//	std::stringstream ss(line.c_str() + 7);
+	//	ss >> mtl_file;
+	//} else if (line.find("usemtl ") == 0 && isUsingMaterials) {
+	//	std::string tname;
+	//	std::stringstream ss(line.c_str() + 7);
+	//	ss >> tname;
+	//	material_name_palette.push_back(tname);
+	//}
   }
 
   // Read .mtl File
